04716663892a6f2b93258fb859f866db9f6017a5
matthew9671 <yixiuz@andrew.cmu.edu>
2015-09-27 16:12:31 -0400

    Create mapController.cs

---
+++ mapController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class mapController : MonoBehaviour {
+	//Yes it's empty
+	//Just to remind you what's missing!
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
